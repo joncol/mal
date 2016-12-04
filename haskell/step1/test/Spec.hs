88f73c7acb02aae813be4e14f3cc819825f02cc4
Jonas Collberg <jonas.collberg@gmail.com>
2016-12-04 16:22:13 +0100

    Fix unit tests

--- haskell/step1/test/Spec.hs
+++ haskell/step1/test/Spec.hs
@@ -12,29 +12,29 @@ unitTests = testGroup "Read and print tests" [atomTests, stringTests]
 atomTests :: TestTree
 atomTests = testGroup "Read and print tests"
   [ testCase "Number atom" $
-      rep "1" @?= "1"
+      rep "1" @?= Right "1"
 
   , testCase "Symbol atom" $
-      rep "abc" @?= "abc"
+      rep "abc" @?= Right "abc"
 
   , testCase "Whitespace" $
-      rep "  123   " @?= "123"
+      rep "  123   " @?= Right "123"
 
   , testCase "List" $
-      rep "(123 456 789)" @?= "(123 456 789)"
+      rep "(123 456 789)" @?= Right "(123 456 789)"
 
   , testCase "String" $
-      rep "\"abc def\"" @?= "\"abc def\""
+      rep "\"abc def\"" @?= Right "\"abc def\""
   ]
 
 stringTests :: TestTree
 stringTests = testGroup "Read and print tests"
   [ testCase "Parens" $
-      rep "\"abc (1 2 3)\"" @?= "\"abc (1 2 3)\""
+      rep "\"abc (1 2 3)\"" @?= Right "\"abc (1 2 3)\""
 
   , testCase "Escaped backslash" $
-      rep "\"abc\\\\123\"" @?= "\"abc\\\\123\""
+      rep "\"abc\\\\123\"" @?= Right "\"abc\\\\123\""
 
   , testCase "Escaped double-quote" $
-      rep "\"abc\\\"123\"" @?= "\"abc\\\"123\""
+      rep "\"abc\\\"123\"" @?= Right "\"abc\\\"123\""
   ]
